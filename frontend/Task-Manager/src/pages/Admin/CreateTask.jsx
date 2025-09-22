5600c2e76bd673b60622761f5a7ff249cb057683
Nguyen Huong Diu <nguyenhuongdiu1710@gmail.com>
2025-09-22 10:17:03 +0700

    integrating delete task api

--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -11,6 +11,8 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import toast from "react-hot-toast";
 import moment from "moment";
+import Modal from "../../components/Modal";
+import DeleteAlert from "../../components/DeleteAlert";
 
 const CreateTask = () => {
   const location = useLocation();
@@ -172,7 +174,19 @@ const CreateTask = () => {
   };
 
   // Delete Task
-  const deleteTask = async () => {};
+  const deleteTask = async () => {
+    try {
+      await axiosInstance.delete(API_PATHS.TASKS.DELETE_TASK(taskId));
+      setOpenDeleteAlert(false);
+      toast.success("Task deleted successfully");
+      navigate("/admin/tasks");
+    } catch (error) {
+      console.error(
+        "Error deleting task:",
+        error?.response?.data?.message || error.message
+      );
+    }
+  };
 
   useEffect(() => {
     if (taskId) {
@@ -313,6 +327,17 @@ const CreateTask = () => {
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={openDeleteAlert}
+        onClose={() => setOpenDeleteAlert(false)}
+        title="Delete Task"
+      >
+        <DeleteAlert
+          content="Are you sure you want to delete this task?"
+          onDelete={() => deleteTask()}
+        />
+      </Modal>
     </DashboardLayout>
   );
 };
